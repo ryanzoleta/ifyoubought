17ab355f9e45a612b23bb9ca59bed4ff26279121
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-10-11 15:38:07 +0800

    initial search ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Button } from '$lib/components/ui/button';
+  import { Input } from '$lib/components/ui/input';
+  import { Search } from 'lucide-svelte';
+</script>
+
+<div class="font-nunito flex flex-col gap-2 self-center p-5">
+  <h1 class="text-center text-5xl font-extrabold tracking-tighter">if you bought</h1>
+
+  <div class="flex flex-row gap-3">
+    <Input placeholder="search for a stock..." />
+    <Button size="icon" class="p-2"><Search size={20} /></Button>
+  </div>
+
+  <div class="flex flex-row justify-center gap-1">
+    <Button variant="outline">AAPL</Button>
+    <Button variant="outline">MSFT</Button>
+    <Button variant="outline">META</Button>
+    <Button variant="outline">NVDA</Button>
+  </div>
+</div>
