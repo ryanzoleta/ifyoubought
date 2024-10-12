46dea7b26ba91b044ec130a3e3f1ed8585b436e4
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-10-13 00:59:22 +0800

    use redis url

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,11 +1,13 @@
 import Redis from 'ioredis';
 import { env } from '$env/dynamic/private';
 
-const redis = new Redis({
-  host: env.REDIS_HOST,
-  port: env.REDIS_POR,
-  username: env.REDIS_USERNAME,
-  password: env.REDIS_PASSWORD
-});
+// const redis = new Redis({
+//   host: env.REDIS_HOST,
+//   port: env.REDIS_POR,
+//   username: env.REDIS_USERNAME,
+//   password: env.REDIS_PASSWORD
+// });
+
+const redis = new Redis(env.REDIS_PUBLIC_URL);
 
 export default redis;
