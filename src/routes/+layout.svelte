3e3cb1cb664b9e640fd0765243428c60fbb75cfb
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-10-12 20:21:20 +0800

    site metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,14 @@
   import '../fonts.css';
 </script>
 
+<svelte:head>
+  <title>Stock Earnings Calculator</title>
+  <meta name="robots" content="index, follow" />
+  <meta name="application-name" content="If You Bought" />
+  <meta name="description" content="Stock Earnings Calculator" />
+  <meta name="keywords" content="Stock Earnings Calculator, FOMO, Stonks, If You Bought" />
+</svelte:head>
+
 <div class="flex min-h-screen flex-col items-center">
   <slot />
 </div>
